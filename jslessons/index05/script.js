7552c3cd3835fbb81b9eb8bef51f80be61a8da2f
Jeremy Wiseman <chema64@gmail.com>
2018-04-30 11:21:27 +1000

    html, css, js content separted and link and script elements added to index5.html

---
+++ jslessons/index05/script.js
@@ -0,0 +1,167 @@
+// ===================== js =====================
+
+// ----------------------------------------------
+/* Using the inputs of numberOfDice and diceSides values each possible roll result or rollOutcome is populated into an array. 
+For example a 6 sided dice roll is stored as 6. This array is then returned for further processing in the  createDiceRollProbability function, which creates a rollCount value. For example if the value of 6 is rolled twice then the collCount value is totaled to the value of 2.  
+*/
+function diceRollResultsListSpesh(numberOfDice, diceSides) {
+
+    //setting oldList to zero allows oldList to be populated during first loop
+    let oldList = [0]
+    let freshList = []
+
+    //number of times dice are rolled
+    for (var nextRoll = 1; nextRoll <= numberOfDice; nextRoll++) {
+        
+  
+
+        // initialize freshList
+        freshList = []
+
+        /*creates all possible dice roll values from dice roll data stored in oldRes array. 
+          freshList is populated with these new results*/
+        oldList.forEach( function(oldRes) {
+            for (var possibleResult = 1; possibleResult <= diceSides; possibleResult++) {
+                freshList.push(oldRes + possibleResult)
+            }
+        })
+        
+  
+
+        // set oldList with latest results set stored in freshList 
+        oldList = freshList
+    }
+
+
+    return oldList
+
+}
+
+// ----------------------------------------------
+/*
+The rawdata array is  processing in the  createDiceRollProbability function, which creates a rollCount value in the resultData. For example if the value of 6 is rolled twice then the collCount value is totaled to the value of 2. 
+*/
+function createDiceRollProbability(rawData) {
+    
+    let resultData = [] 
+
+    rawData.forEach(function(element) {
+
+    if(resultData[element]) {
+
+        resultData[element] += 1;
+    }
+    else
+    {
+
+        resultData[element] = 1;
+    }
+
+
+    });
+
+    return resultData
+
+}
+
+// ----------------------------------------------
+/*
+Create a div element and return div for use in printProbabilityTable function
+*/
+function makeDiv(divLength) {
+    const div = document.createElement("div")
+    div.style.width = divLength + "em"
+    return div
+}
+
+// ----------------------------------------------
+    /* Two values relating to the probability of dice rolls are tallied here from the resultData. Firstly all possible roll values 
+    in resultData are stored as a array index value, which below is called rollOutcome. For example the rollOutcome value could be 
+    6 when using a six sided dice, for one roll. Secondly the number of times that rollOutcome occurs is stored in the 
+    rollCount value. 
+
+    totalRollCount: Tallies all rollCount values for each rollOutome.
+    highestRollCount: Finds the highest rollCount value from all rollOutome values.
+    
+    These values are then used to populate a table below with the following column values:
+    
+    -rollOutcome
+    -ratio on rollOutcome to totalRollCount
+    -percentage on rollOutcome
+    -div bar chart
+
+    */ 
+function printProbabilityTable(resultData) {
+
+    var table = document.createElement("table")
+
+    var totalRollCount = 0
+    var highestRollCount = 0
+
+    //Find the totalRollCount and highestRollCount from rollCount
+    resultData.forEach(function(rollCount) {
+        
+        totalRollCount += rollCount
+        if( highestRollCount < rollCount) {
+
+             highestRollCount = rollCount
+
+        }
+        
+         
+    })
+
+
+    
+    //create a table to display rollCount results
+    resultData.forEach(function(rollCount, rollOutcome) {
+
+        // make the row
+        var tr = document.createElement("tr")
+        table.appendChild(tr)
+
+        // do the rollOutcome cell
+        var rollOutcomeCell = document.createElement("td")
+        rollOutcomeCell.innerHTML = rollOutcome
+        tr.appendChild(rollOutcomeCell)
+
+        // do the ratio cell
+        var ratioCell = document.createElement("td")
+        ratioCell.innerHTML = rollCount  + '/' + totalRollCount
+        tr.appendChild(ratioCell)
+
+        // do the percentage cell
+        var ratioCell = document.createElement("td")
+        ratioCell.innerHTML = Math.round(rollCount / totalRollCount * 1000)/10 + '%'
+        tr.appendChild(ratioCell)
+
+
+        // do the graph bar cell
+        var divCell = document.createElement("td")
+        var div = makeDiv(rollCount)
+
+        var divWidth = rollCount /  highestRollCount * 100
+        div.style.width = divWidth + "px"
+
+        //attach div element to table cell 
+        divCell.appendChild(div)
+        tr.appendChild(divCell)
+
+    })
+
+    document.body.appendChild(table)
+}
+
+// ----------------------------------------------
+
+var rawData = []
+rawData = diceRollResultsListSpesh(2, 6)
+
+var resultData = [] 
+resultData = createDiceRollProbability(rawData)
+
+
+printProbabilityTable(resultData)
+
+
+// ===================== js =====================
